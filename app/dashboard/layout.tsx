b7043ed47e4a2f6b17611d186578ada741ca8e45
nasriime <mnasr4040@gmail.com>
2024-09-16 07:40:55 +0200

    save user

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,6 +1,40 @@
 import { redirect } from "next/navigation";
 import DashboardNav from "../components/DashboardNav";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import prisma from "../lib/db";
+
+async function getData({
+  email,
+  id,
+  firstName,
+  lastName,
+}: {
+  email: string;
+  id: string;
+  firstName: string | undefined | null;
+  lastName: string | undefined | null;
+  profileImage: string | undefined | null;
+}) {
+  const user = await prisma.user.findUnique({
+    where: {
+      id: id,
+    },
+    select: {
+      id: true,
+    },
+  });
+
+  if (!user) {
+    const name = `${firstName ?? ""} ${lastName ?? ""}`;
+    await prisma.user.create({
+      data: {
+        id: id,
+        email: email,
+        name: name,
+      },
+    });
+  }
+}
 
 export default async function DashboardLayout({
   children,
@@ -13,6 +47,14 @@ export default async function DashboardLayout({
   if (!user) {
     return redirect("/");
   }
+
+  await getData({
+    email: user.email as string,
+    firstName: user.given_name as string,
+    id: user.id as string,
+    lastName: user.family_name as string,
+    profileImage: user.picture,
+  });
 
   return (
     <div className="flex flex-col items-center space-y-6 mt-10">
