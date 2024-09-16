bfe36bf6f4798fe9deae33283ff94a8075f3c4b0
nasriime <mnasr4040@gmail.com>
2024-09-16 15:51:27 +0200

    complete crud operation

---
+++ app/dashboard/new/page.tsx
@@ -0,0 +1,82 @@
+import { SubmitButton } from "@/app/components/Submitbuttons";
+import prisma from "@/app/lib/db";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Textarea } from "@/components/ui/textarea";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import Link from "next/link";
+import { redirect } from "next/navigation";
+
+export default async function NewNoteRoute() {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+  async function postData(formData: FormData) {
+    "use server";
+
+    if (!user) {
+      throw new Error("Not authorized");
+    }
+
+    const title = formData.get("title") as string;
+    const content = formData.get("content") as string;
+
+    await prisma.note.create({
+      data: {
+        userId: user?.id,
+        content: content,
+        version: 1,
+        title: title,
+      },
+    });
+
+    return redirect("/dashboard");
+  }
+  return (
+    <Card>
+      <form action={postData}>
+        <CardHeader>
+          <CardTitle>New Note</CardTitle>
+          <CardDescription>
+            Right here you can now create your new notes
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="flex flex-col gap-y-5">
+          <div className="gap-y-2 flex flex-col">
+            <Label>Title</Label>
+            <Input
+              required
+              type="text"
+              name="title"
+              placeholder="Title for your note"
+            />
+          </div>
+
+          <div className="flex flex-col gap-y-2">
+            <Label>Content</Label>
+            <Textarea
+              name="content"
+              placeholder="Describe your note as you want"
+              required
+            />
+          </div>
+        </CardContent>
+
+        <CardFooter className="flex justify-between">
+          <Button asChild variant="destructive">
+            <Link href="/dashboard">Cancel</Link>
+          </Button>
+          <SubmitButton />
+        </CardFooter>
+      </form>
+    </Card>
+  );
+}
