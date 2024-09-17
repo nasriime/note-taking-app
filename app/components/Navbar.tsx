240ebb87872564d4910296c8c979e62d9e923929
nasriime <mnasr4040@gmail.com>
2024-09-17 09:46:19 +0200

    add Logo

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -7,18 +7,17 @@ import {
 } from "@kinde-oss/kinde-auth-nextjs/components";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import UserNav from "./UserNav";
+import { NotebookPen } from "lucide-react";
 
 export async function Navbar() {
   const { isAuthenticated, getUser } = getKindeServerSession();
   const user = await getUser();
-  
+
   return (
     <nav className="border-b bg-background h-[10vh] flex items-center justify-center">
       <div className="container flex items-center justify-between">
         <Link href="/">
-          <h1 className="font-bold text-3xl">
-            Note <span className="text-primary">Taking</span>
-          </h1>
+          <NotebookPen className="w-10 h-10 text-primary" />
         </Link>
         <div className="flex items-center gap-x-5">
           <ThemeToggle />
